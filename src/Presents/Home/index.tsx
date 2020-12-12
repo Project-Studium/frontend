6fe5c0e65b6ffc77ae795ebaf3261293da9beb21
Maria Ribeiro <mariaeduardaolivribeiro@gmail.com>
2020-12-11 22:09:16 -0300

    Added the images

--- src/Presents/Home/index.tsx
+++ src/Presents/Home/index.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 
+import BackgroundImage from '../../assets/SUFOCO.jpg'
+import Hat from '../../assets/Chapéu.svg'
+import WaveOne from '../../assets/OndaUm.svg'
+import WaveTwo from '../../assets/OndaDois.svg'
+
 const HomePage = () => {
     return (
         <div>
-           <h1>HomePage Studium</h1> 
+            <img src={BackgroundImage} />
+            <img src={Hat} />
+            <img src={WaveOne} />
+            <img src={WaveTwo} />
             <Link to='/signin'>fazer login</Link>
         </div>
     )
